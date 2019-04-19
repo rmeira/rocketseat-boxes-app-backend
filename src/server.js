b86bfd8db8d75dab64d023f65cfbc11d9829db3f
Rafael Meira <rafaelmeira@me.com>
2019-04-19 15:18:57 -0300

    cors error on server

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 
 const server = require("http").Server(app);
-const io = require("socket.io")(server);
+const io = require("socket.io")(server, { origins: "*:*" });
 
 io.on("connection", socket => {
   socket.on("connectRoom", box => {
