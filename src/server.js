345f347086cae10afb1b5c790aeae7b47e11cb9a
Rafael Meira <rafaelmeira@me.com>
2019-04-19 20:02:46 -0300

    try correct cors

--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 
 const app = express();
 
-const server = require("http").Server(app);
+const server = require("http").createServer(app);
 const io = require("socket.io")(server);
 
 app.use(cors());
