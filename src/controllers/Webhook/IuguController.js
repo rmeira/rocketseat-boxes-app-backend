d0e145b7d5aeff3043363fb9664c9a12701694bf
Rafael Meira <rafael.meira@hsm.com.br>
2019-05-14 11:52:21 -0300

    change order data

--- src/controllers/Webhook/IuguController.js
+++ src/controllers/Webhook/IuguController.js
@@ -18,7 +18,7 @@ class IuguController {
 
     console.log(order.data);
 
-    await Order.create(order.data);
+    await Order.create(JSON.stringify(order.data));
     await Iugu.create(req.body);
 
     return res.json('Order created');
